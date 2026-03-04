9481a1d0682ffdb73f0440242ffdbb48fa1318a2
FenuPhilip <150925717+FenuPhilip@users.noreply.github.com>
2026-03-04 17:26:38 +0000

    add external_url field to post_uni_correction_lnk_yt2

---
+++ feed/templatetags/video_filters.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def youtube_id(url):
+    if "watch?v=" in url:
+        return url.split("watch?v=")[1].split("&")[0]
+    if "youtu.be/" in url:
+        return url.split("youtu.be/")[1].split("?")[0]
+    return ""
